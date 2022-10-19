69a390725fd5bc60c0c7850abeadfb551d732584
oguchi somto <oguchi.somto@gmail.com>
2022-10-19 18:21:00 +0100

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,10 +4,10 @@
 #include <stdio.h>
 
 /*
- *  * File: main.h
- *   *
- *    * Desc: header file
- *    **/
+ * File: main.h
+ *
+ * Desc: header file
+**/
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
